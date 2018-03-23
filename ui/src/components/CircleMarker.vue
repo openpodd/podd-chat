5f54210730fdb86fb7c326e28b7fdbe7cbafeef4
polawat phetra <pphetra@gmail.com>
2018-03-23 15:24:32 +0700

    fix uglify error -> Invalid assignment.

--- ui/src/components/CircleMarker.vue
+++ ui/src/components/CircleMarker.vue
@@ -7,8 +7,6 @@
 <script>
   /* eslint-disable eqeqeq */
 
-  import eventsBinder from '../../node_modules/vue2-leaflet/src/utils/eventsBinder'
-  import propsBinder from '../../node_modules/vue2-leaflet/src/utils/propsBinder'
   import L from 'leaflet'
 
   const events = [
@@ -25,6 +23,50 @@
     'tooltipopen',
     'tooltipclose'
   ]
+
+  let eventsBinder = (vueElement, leaflet, events) => {
+    for (var i = 0; i < events.length; i++) {
+      const exposedName = 'l-' + events[i]
+      const eventName = events[i]
+      leaflet.on(eventName, (ev) => {
+        vueElement.$emit(exposedName, ev)
+      })
+    }
+  }
+
+  function capitalizeFirstLetter (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  let propsBinder = (vueElement, leafletElement, props, options) => {
+    const keys = Object.keys(props)
+    for (var i = 0; i < keys.length; i++) {
+      const key = keys[i]
+      const setMethodName = 'set' + capitalizeFirstLetter(key)
+      const deepValue = (props[key].type === Object) ||
+        (props[key].type === Array) ||
+        (Array.isArray(props[key].type))
+      if (props[key].custom) {
+        vueElement.$watch(key, (newVal, oldVal) => {
+          vueElement[setMethodName](newVal, oldVal)
+        }, {
+          deep: deepValue
+        })
+      } else if (setMethodName == 'setOptions') {
+        vueElement.$watch(key, (newVal, oldVal) => {
+          L.setOptions(leafletElement, newVal)
+        }, {
+          deep: deepValue
+        })
+      } else {
+        vueElement.$watch(key, (newVal, oldVal) => {
+          leafletElement[setMethodName](newVal)
+        }, {
+          deep: deepValue
+        })
+      }
+    }
+  }
 
   const props = {
     tooltip: {
